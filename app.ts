937e45810185dff5168ebfc3d54ff605fa1a904c
rosen-vladimirov <rosen.vladimirov@telerik.com>
2016-02-23 09:18:03 +0200

    Use new syntax for application.start
    Use the new syntax for application.start method, so set minimum required runtime versions to 1.6.0.

--- app.ts
+++ app.ts
@@ -1,4 +1,2 @@
-import application = require("application");
-application.mainModule = "./tns_modules/nativescript-unit-test-runner/main-page";
-application.cssFile = "./tns_modules/nativescript-unit-test-runner/app.css";
-application.start();
+import {application} from "application";
+application.start({ moduleName: "./tns_modules/nativescript-unit-test-runner/main-page" });
