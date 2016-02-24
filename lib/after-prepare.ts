4990f91c288cbfa1a4bf592c28b84c991e4a1965
rosen-vladimirov <rosen.vladimirov@telerik.com>
2016-02-24 22:30:45 +0200

    Add after-prepare hook to change the entry-point of application
    Add after-prepare hook that will change the entry point of application in case `test` command is executed.
    Use `nativescript-hook` dependency in order to install the hook.

---
+++ lib/after-prepare.ts
@@ -0,0 +1,15 @@
+import * as path from "path";
+import * as fs from "fs";
+
+module.exports = function ($platformsData, $testExecutionService) {
+	if($testExecutionService && $testExecutionService.platform) {
+		let platformData = $platformsData.getPlatformData($testExecutionService.platform),
+			projectFilesPath = path.join(platformData.appDestinationDirectoryPath, "app"),
+			packageJsonPath = path.join(projectFilesPath, 'package.json'),
+			packageJson = JSON.parse(fs.readFileSync(packageJsonPath).toString());
+
+		// When test command is used in ns-cli, we should change the entry point of the application
+		packageJson.main = "./tns_modules/nativescript-unit-test-runner/app.js";
+		fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson));
+	}
+}
