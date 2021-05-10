a0be482a3b6267fccf1e92ec5eaeffbe92c2acaf
Nathan Walker <walkerrunpdx@gmail.com>
2021-05-10 13:17:03 -0700

    chore: npm 7 compat

--- makeTestApp.sh
+++ makeTestApp.sh
@@ -8,9 +8,9 @@ cd test-app
 
 ns test init --framework jasmine
 
-npm i ../nativescript-unit-test-runner-*.tgz
+npm i ../nativescript-unit-test-runner-*.tgz --legacy-peer-deps
 # todo: change...
-npm i ~/Code/NativeScript/packages/webpack5/nativescript-webpack-5.0.0-dev.tgz
+npm i ~/Code/NativeScript/packages/webpack5/nativescript-webpack-5.0.0-dev.tgz --legacy-peer-deps
 
 rm webpack.config.js
 ./node_modules/.bin/nativescript-webpack init
