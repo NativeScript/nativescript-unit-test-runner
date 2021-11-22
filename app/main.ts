eed3e59782d219876add81bc12a3ebbc69318d48
Eduardo Speroni <edusperoni@gmail.com>
2021-11-22 18:43:07 -0300

    chore: cleanup

--- app/main.ts
+++ app/main.ts
@@ -1,27 +1,21 @@
-
 declare let require: any;
 
 import { Application } from "@nativescript/core";
-import "./app.css"
+import "./app.css";
 import { registerTestRunner } from "./services/webpack-test-runner";
 
-const context = require.context('./', true, /.*\.(js|css|xml)/)
+const context = require.context("./", true, /.*\.(js|css|xml)/);
 global.registerWebpackModules(context);
 
 export interface RunTestAppOptions {
-    runTests?: () => unknown
+  runTests?: () => unknown;
 }
 
-
 export function runTestApp(options: RunTestAppOptions = {}) {
-    if(options?.runTests) {
-        registerTestRunner(() => {
-            try {
-                options.runTests();
-            } catch (e) {
-                console.error('Error loading tests', e);
-            }
-        });
-    }
-    Application.run({ moduleName: "bundle-app-root" });
+  if (options?.runTests) {
+    registerTestRunner(() => {
+      options.runTests();
+    });
+  }
+  Application.run({ moduleName: "bundle-app-root" });
 }
