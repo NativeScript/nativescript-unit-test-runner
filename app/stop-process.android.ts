027dadba2e4a3e5be3d24b61c218b16f7da5f003
Eduardo Speroni <edusperoni@gmail.com>
2022-03-21 10:04:19 -0300

    fix(android): properly force quit the application

--- app/stop-process.android.ts
+++ app/stop-process.android.ts
@@ -1,3 +1,10 @@
+import { Application } from '@nativescript/core';
+
 export function killProcess() {
+    if(android.os.Build.VERSION.SDK_INT >= 21) {
+        Application.android.startActivity?.finishAndRemoveTask();
+    } else {
+        Application.android.startActivity?.finishAffinity();
+    }
     java.lang.System.exit(0);
 }
