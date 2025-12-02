8cc7a0a6440ab35593ab1d69bdad492e5174db55
Tyler Hendrickson <6372535+hendrickson-tyler@users.noreply.github.com>
2025-12-02 16:00:25 -0700

    fix: use proper ES Module syntax

--- app/bundle-main-page.ts
+++ app/bundle-main-page.ts
@@ -1,6 +1,5 @@
 import { mainViewModel} from "./main-view-model";
-function pageLoaded(args) {
+export function pageLoaded(args) {
     var page = args.object;
     page.bindingContext = mainViewModel;
 }
-exports.pageLoaded = pageLoaded;
